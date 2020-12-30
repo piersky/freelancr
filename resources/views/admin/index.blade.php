9d75608df9b22348d1dbd815c8b7cec91ad023a8
Pier Luigi Papeschi <papeschi@gmail.com>
2020-12-30 21:09:55 +0100

    Bug fixes.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,40 +2,42 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-header">
-                <h4>{{__('Jobs Due')}}</h4>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">{{__('Jobs for')}} {{Auth::user()->name}}</h5>
-                <table>
-                @foreach($jobs as $job)
-                    <tr>
-                        <td class="text-left px-3">{{date('d/m/Y H:i', strtotime($job->deadline))}}</td>
-                        <td class="text-left text-uppercase">{{$job->description}}</td>
-                        <td class="text-leftr px-3">{{$job->customer_name}}</td>
-                        <td><a href="#" class="btn btn-primary pl-3">Go somewhere</a></td>
-                    </tr>
-                @endforeach
-                </table>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header">
-                <h4>{{__('Last activities')}}</h4>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">{{__('Jobs for')}} {{Auth::user()->name}}</h5>
-                <table>
+        <div class="row">
+            <div class="card col-sm-6">
+                <div class="card-header">
+                    <h4>{{__('Jobs Due')}}</h4>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{__('Jobs for')}} {{Auth::user()->name}}</h5>
+                    <table>
                     @foreach($jobs as $job)
                         <tr>
                             <td class="text-left px-3">{{date('d/m/Y H:i', strtotime($job->deadline))}}</td>
                             <td class="text-left text-uppercase">{{$job->description}}</td>
                             <td class="text-leftr px-3">{{$job->customer_name}}</td>
-                            <td><a href="#" class="btn btn-primary pl-3">Go somewhere</a></td>
                         </tr>
                     @endforeach
-                </table>
+                    </table>
+                </div>
+                <div class="card-footer bg-transparent border-success">Footer</div>
+            </div>
+            <div class="card col-sm-6">
+                <div class="card-header">
+                    <h4>{{__('Last activities')}}</h4>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{__('Jobs for')}} {{Auth::user()->name}}</h5>
+                    <table>
+                        @foreach($jobs as $job)
+                            <tr>
+                                <td class="text-left px-3">{{date('d/m/Y H:i', strtotime($job->deadline))}}</td>
+                                <td class="text-left text-uppercase">{{$job->description}}</td>
+                                <td class="text-leftr px-3">{{$job->customer_name}}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+                <div class="card-footer bg-transparent border-success">Footer</div>
             </div>
         </div>
     </div>
